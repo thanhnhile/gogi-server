da1adcf4135558ecafdfd11301e323408fc1a91b
thanhnhile <marrythanhnhipl205@gmail.com>
2022-10-02 21:25:30 +0700

    init

---
+++ src/main/java/vn/com/gigo/GigoApplication.java
@@ -0,0 +1,13 @@
+package vn.com.gigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/gigo/GigoApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.gigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
