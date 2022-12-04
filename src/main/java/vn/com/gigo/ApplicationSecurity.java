a8df14c2c224a8cc1be8aed06e7df8bd35b00c42
thanhnhile <marrythanhnhipl205@gmail.com>
2022-12-04 08:50:50 +0700

    config cors

--- src/main/java/vn/com/gigo/ApplicationSecurity.java
+++ src/main/java/vn/com/gigo/ApplicationSecurity.java
@@ -1,9 +1,12 @@
 package vn.com.gigo;
 
+import java.util.Arrays;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,14 +18,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import vn.com.gigo.repositories.AccountRepository;
 import vn.com.gigo.security.JwtTokenFilter;
 
 
-
+@Configuration
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@CrossOrigin(value = "http://localhost:3000")
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
 	@Autowired private AccountRepository accountRepo;
@@ -42,6 +50,7 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and();
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		 http
@@ -61,7 +70,19 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
         
 		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 	}
+	 @Bean
+	    CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+	        configuration.addAllowedHeader("*");
+	        configuration.addAllowedOrigin("*");
+	        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
 
+	@SuppressWarnings("deprecation")
 	@Override
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
